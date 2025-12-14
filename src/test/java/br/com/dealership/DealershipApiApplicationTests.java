774fd176b3a6b5fd60eacee8461425a5947741a3
Matheus Zacarias <matheuus412@gmail.com>
2025-12-14 17:13:03 -0300

    feat: first configs

---
+++ src/test/java/br/com/dealership/DealershipApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dealership;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DealershipApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
