2475329de46e4746d54bd3dba30675007072280b
Matheus Zacarias <matheuus412@gmail.com>
2025-12-14 20:59:54 -0300

    feat: use cases

---
+++ src/main/java/br/com/dealership/modules/vehicle/application/usecases/CreateVehicleUseCase.java
@@ -0,0 +1,28 @@
+package br.com.dealership.modules.vehicle.application.usecases;
+
+import br.com.dealership.modules.vehicle.domain.entities.Vehicle;
+import br.com.dealership.modules.vehicle.domain.ports.out.VehicleRepositoryPort;
+
+import java.util.UUID;
+
+public class CreateVehicleUseCase {
+    private final VehicleRepositoryPort vehicleRepositoryPort;
+
+    public CreateVehicleUseCase(VehicleRepositoryPort vehicleRepositoryPort) {
+        this.vehicleRepositoryPort = vehicleRepositoryPort;
+    }
+
+    public Vehicle execute(Vehicle vehicle) {
+        vehicle.validate();
+
+        if (vehicle.getId() == null) {
+            vehicle.setId(UUID.randomUUID());
+        }
+
+        if (vehicleRepositoryPort.getVehicleByVin(vehicle.getVin()).isPresent()) {
+            throw new IllegalArgumentException("Vehicle with VIN " + vehicle.getVin() + " already exists");
+        }
+
+        return vehicleRepositoryPort.saveVehicle(vehicle);
+    }
+}
