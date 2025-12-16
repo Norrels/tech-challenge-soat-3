8899a07651cc8df596f7abdedca1bfb6df0a8211
Matheus Zacarias <matheuus412@gmail.com>
2025-12-16 20:07:16 -0300

    refactor: vehicle service

--- src/main/java/br/com/dealership/modules/vehicle/application/services/VehicleService.java
+++ src/main/java/br/com/dealership/modules/vehicle/application/services/VehicleService.java
@@ -3,22 +3,26 @@ package br.com.dealership.modules.vehicle.application.services;
 import br.com.dealership.modules.vehicle.application.usecases.CreateVehicleUseCase;
 import br.com.dealership.modules.vehicle.application.usecases.GetVehicleByStatusUseCase;
 import br.com.dealership.modules.vehicle.application.usecases.GetVehicleByVinUseCase;
+import br.com.dealership.modules.vehicle.application.usecases.UpdateVehicleUseCase;
 import br.com.dealership.modules.vehicle.domain.entities.Vehicle;
 import br.com.dealership.modules.vehicle.domain.entities.VehicleStatus;
 import br.com.dealership.modules.vehicle.domain.ports.in.VehicleServicePort;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
 public class VehicleService implements VehicleServicePort {
     private final CreateVehicleUseCase createVehicleUseCase;
     private final GetVehicleByVinUseCase getVehicleByVinUseCase;
     private final GetVehicleByStatusUseCase getVehicleByStatusUseCase;
+    private final UpdateVehicleUseCase updateVehicleUseCase;
 
-    public VehicleService(CreateVehicleUseCase createVehicleUseCase, GetVehicleByVinUseCase getVehicleByVinUseCase, GetVehicleByStatusUseCase getVehicleByStatusUseCase) {
+    public VehicleService(CreateVehicleUseCase createVehicleUseCase, GetVehicleByVinUseCase getVehicleByVinUseCase, GetVehicleByStatusUseCase getVehicleByStatusUseCase, UpdateVehicleUseCase updateVehicleUseCase) {
         this.createVehicleUseCase = createVehicleUseCase;
         this.getVehicleByVinUseCase = getVehicleByVinUseCase;
         this.getVehicleByStatusUseCase = getVehicleByStatusUseCase;
+        this.updateVehicleUseCase = updateVehicleUseCase;
     }
 
     @Override
@@ -34,5 +38,10 @@ public class VehicleService implements VehicleServicePort {
     @Override
     public Vehicle createVehicle(Vehicle vehicle) {
         return createVehicleUseCase.execute(vehicle);
+    }
+
+    @Override
+    public Vehicle updateVehicle(UUID id, Vehicle vehicle) {
+        return updateVehicleUseCase.execute(id, vehicle);
     }
 }
