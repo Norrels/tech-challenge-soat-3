56e0e24bf2bbdfd34e6aed887e65cbb5512c862f
Matheus Zacarias <matheuus412@gmail.com>
2025-12-24 18:50:22 -0300

    feat: sale config

---
+++ src/main/java/br/com/dealership/modules/sale/config/SaleConfig.java
@@ -0,0 +1,48 @@
+package br.com.dealership.modules.sale.config;
+
+import br.com.dealership.modules.sale.application.services.SaleService;
+import br.com.dealership.modules.sale.application.useCases.CreateSaleUseCase;
+import br.com.dealership.modules.sale.application.useCases.FindAllSaleByCustomerCPFUseCase;
+import br.com.dealership.modules.sale.application.useCases.FindAllSalesUseCase;
+import br.com.dealership.modules.sale.application.useCases.FindSaleByIdUseCase;
+import br.com.dealership.modules.sale.domain.ports.out.SaleRepositoryPort;
+import br.com.dealership.modules.sale.mapper.SaleMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SaleConfig {
+
+    @Bean
+    public SaleMapper saleMapper () {
+        return new SaleMapper();
+    }
+
+    @Bean
+    public CreateSaleUseCase createSaleUseCase(SaleRepositoryPort repositoryPort) {
+        return new CreateSaleUseCase(repositoryPort);
+    }
+
+    @Bean
+    public FindAllSaleByCustomerCPFUseCase findAllSaleByCustomerCPFUseCase(SaleRepositoryPort repositoryPort) {
+        return new FindAllSaleByCustomerCPFUseCase(repositoryPort);
+    }
+
+    @Bean
+    public FindSaleByIdUseCase findSaleByIdUseCase(SaleRepositoryPort repositoryPort) {
+        return new FindSaleByIdUseCase(repositoryPort);
+    }
+
+    @Bean
+    public FindAllSalesUseCase findAllSalesUseCase(SaleRepositoryPort repositoryPort) {
+        return new FindAllSalesUseCase(repositoryPort);
+    }
+
+    @Bean
+    public SaleService saleService(CreateSaleUseCase createSaleUseCase,
+                                   FindSaleByIdUseCase findSaleByIdUseCase,
+                                   FindAllSalesUseCase findAllSalesUseCase,
+                                   FindAllSaleByCustomerCPFUseCase findAllSaleByCustomerCPFUseCase) {
+        return new SaleService(createSaleUseCase, findSaleByIdUseCase, findAllSalesUseCase, findAllSaleByCustomerCPFUseCase);
+    }
+}
