11e2e46ed8357c3b8ef2cc04e0478e677c4dc2b5
Matheus Zacarias <matheuus412@gmail.com>
2025-12-24 17:07:36 -0300

    feat: sale repository port

---
+++ src/main/java/br/com/dealership/modules/sale/domain/ports/out/SaleRepositoryPort.java
@@ -0,0 +1,11 @@
+package br.com.dealership.modules.sale.domain.ports.out;
+
+import br.com.dealership.modules.sale.domain.entities.SaleOrder;
+
+import java.util.List;
+
+public interface SaleRepositoryPort {
+    SaleOrder save(SaleOrder saleOrder);
+    List<SaleOrder> getAllSales(String cpfClient);
+    SaleOrder getSaleById(String id);
+}
