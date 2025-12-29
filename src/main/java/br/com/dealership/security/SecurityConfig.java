a745311dfc586e12bd5e5b048ff1c7a6c6f805df
Matheus Zacarias <matheuus412@gmail.com>
2025-12-29 01:14:08 -0300

    remove: load balance and cognito config

--- src/main/java/br/com/dealership/security/SecurityConfig.java
+++ src/main/java/br/com/dealership/security/SecurityConfig.java
@@ -1,6 +1,5 @@
 package br.com.dealership.security;
 
-import br.com.dealership.security.alb.filters.AlbJwtHeaderFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -11,29 +10,24 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
 @Profile("!dev")
 public class SecurityConfig {
-    private final AlbJwtHeaderFilter albJwtHeaderFilter;
-
-    public SecurityConfig(AlbJwtHeaderFilter albJwtHeaderFilter) {
-        this.albJwtHeaderFilter = albJwtHeaderFilter;
-    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)
             .authorizeHttpRequests(authorize -> authorize
                 .requestMatchers("/health").permitAll()
+                .requestMatchers("/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
                 .anyRequest().authenticated()
-            )   .addFilterBefore(albJwtHeaderFilter, UsernamePasswordAuthenticationFilter.class)
-                .oauth2ResourceServer(oauth -> oauth
-                        .jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter()))
-                );
+            )
+            .oauth2ResourceServer(oauth -> oauth
+                .jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter()))
+            );
         return http.build();
     }
 
